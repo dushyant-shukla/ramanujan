1348afa72c83418c9e474adabf704728e0a400dc
dushyant-shukla <dushyantshukla@outlook.com>
2022-10-07 06:04:17 -0700

    Added a method to Quaternion class.

--- source/quaternion.cpp
+++ source/quaternion.cpp
@@ -1,6 +1,26 @@
 #include "quaternion.h"
 
+#include <cmath>
+
 namespace ramanujan
 {
+
+Quaternion Quaternion::AngleAxis(float theta, const Vector3& axis)
+{
+    // Quaternions are created using an angle and an axis of rotation. A rotation of angle(theta) about an axis, can be
+    // represented on a sphere as any directed arc whose length us 1/2 of angle(theta) on the plane perpendicular to the
+    // rotation axis. Positive angles yield a counterclockwise rotation around the axis.
+    Vector3 normalized_axis = normalized(axis);
+
+    // A quaternion can track two full rotations, which is 720 degrees. This makes the period of a quaternion 720
+    // degrees. The period of sin/cos is 360 degrees. Dividing angle(theta) by 2 maps the range of a quaternion to the
+    // range of sin/cos.
+    float sin_theta = sinf(theta * 0.50f);
+
+    return Quaternion(normalized_axis.x * sin_theta,
+                      normalized_axis.y * sin_theta,
+                      normalized_axis.z * sin_theta,
+                      cosf(theta * 0.50f));
+}
 
 } // namespace ramanujan
--- source/quaternion.h
+++ source/quaternion.h
@@ -5,6 +5,9 @@
 namespace ramanujan
 {
 
+/**
+ * Quaternions are used to encode rotation about an arbitrary axis.
+ */
 struct Quaternion
 {
     union
@@ -25,6 +28,22 @@ struct Quaternion
 
         float v[4];
     };
+
+    /**
+     * Constructs an identity quaternion.
+     */
+    inline Quaternion() : x(0), y(0), z(0), w(1) {}
+
+    inline Quaternion(float _x, float _y, float _z, float _w) : x(_x), y(_y), z(_z), w(_w) {}
+
+    /**
+     * This method encodes an angle and an axis of rotation into a quaternion.
+     *
+     * \param theta The angle of rotation
+     * \param axis The axis of rotation
+     * \return A quaternion representing the rotation
+     */
+    Quaternion AngleAxis(float theta, const Vector3& axis);
 };
 
 } // namespace ramanujan
