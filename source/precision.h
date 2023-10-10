59d50a7e5ab7489d17ee4dc34fd0ab85d17546db
dushyant-shukla <dushyantshukla@outlook.com>
2023-10-10 14:42:03 -0700

    Adding experimental vector class (refactoring the math library).

--- source/precision.h
+++ source/precision.h
@@ -2,9 +2,21 @@
 
 namespace ramanujan
 {
+
+#if 1
+
 /*
  * Defines a real number precision. This provides us the flexibility to compile the library in single or a double
  * floating-point precision.
  */
 using real = float;
+
+real pow(real x, real y) {}
+
+#define real_pow(x, y) powf(x, y)
+
+#else
+
+#endif
+
 } // namespace ramanujan
